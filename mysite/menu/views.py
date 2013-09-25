4ca325a0f0181f36a0dd6305cdd8e32a240b53ee
Sushila Burgess <sushila.burgess@bodleian.ox.ac.uk>
2013-09-25 15:00:43 +0000

    Set up a new Solr "conf" directory with the files that were already in live use on the server.
    git-svn-id: https://damssupport.bodleian.ox.ac.uk/svn/mlgb3/trunk@38 a75bfd90-2fc9-4c99-ad46-084cd6553aa8

--- mysite/menu/views.py
+++
@@ -1,9 +0,0 @@
-from django.template import Context, loader
-from django.http import HttpResponse,Http404,HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render_to_response
-from django.core.urlresolvers import reverse
-from django.utils.html import escape
-from urllib import quote, unquote
-
-def index(request):
-    return render_to_response('menu/index.html')
--- mysite/mlgb/admin.py
+++
@@ -1,6 +0,0 @@
-#from django.contrib import admin
-#from mysite.mlgb.views import mlgb
-
-#admin.site.register(mlgb)
-
-
